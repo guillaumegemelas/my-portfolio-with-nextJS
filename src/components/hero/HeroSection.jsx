15e8c804cb4a52036a824dc2b993924a63f97f83
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-09-28 23:29:01 +0200

    add cv

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -1,5 +1,10 @@
 import { FULL_NAME } from "../../lib/config";
 
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { RiContactsLine } from "react-icons/ri";
+
 export const HeroSection = () => {
   return (
     <>
@@ -30,17 +35,44 @@ export const HeroSection = () => {
           </p>
 
           <p className="mt-36 max-w-xl text-xl dark:drop-shadow-[0_0px_5px_rgba(0,0,0,1)]">
-            <p className="mb-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-4xl font-extrabold text-transparent">
-              What can I do for you.
-            </p>
-            <b>I develop your websites</b> on both the client-side (Front-end)
-            and server-side (Back-end). Below, you can find my projects, most of
-            which have been created using ReactJS or NextJS.
+            <span className="mb-3 flex items-center">
+              <span className="mr-4 bg-gradient-to-r from-primary to-secondary bg-clip-text text-4xl font-extrabold">
+                <AiOutlineQuestionCircle
+                  style={{
+                    color: "#677AF1",
+                  }}
+                />
+              </span>{" "}
+              <span className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-4xl font-extrabold text-transparent">
+                What can I do for you
+              </span>{" "}
+            </span>
+            <br />
+            <br />
+            <span className="flex items-center">
+              {" "}
+              {/* Use a flex container */}
+              <span className="text-4xl font-extrabold ">
+                <HiOutlineDesktopComputer />
+              </span>{" "}
+              <span className="ml-7">
+                <b>I develop your websites</b> on both the client-side
+                (Front-end) and server-side (Back-end). Below, you can find my
+                projects, most of which have been created using ReactJS or
+                NextJS.
+              </span>
+            </span>
           </p>
-          <p className="mt-3 max-w-xl text-xl dark:drop-shadow-[0_0px_5px_rgba(0,0,0,1)]">
-            Do not hesitate to contact me, I would be happy to review your
-            project and respond as soon as possible
-          </p>
+          <span className="flex items-center">
+            <span className="text-4xl font-extrabold">
+              {" "}
+              <RiContactsLine />
+            </span>
+            <span className="mt-3 ml-7 max-w-xl text-xl dark:drop-shadow-[0_0px_5px_rgba(0,0,0,1)]">
+              Do not hesitate to contact me, I would be happy to review your
+              project and respond as soon as possible
+            </span>
+          </span>
         </div>
       </div>
     </>
