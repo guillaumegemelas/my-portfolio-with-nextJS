48a15cee42b0b4649fe307aea7d606ea410dece5
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-10-05 15:44:15 +0200

    add icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
-import { FULL_NAME, SOCIAL_NETWORKS } from "../lib/config";
+import {
+  //FULL_NAME,
+  SOCIAL_NETWORKS,
+} from "../lib/config";
 import { SocialNetworks } from "./atom/SocialNetwork";
 import { ToggleThemeButton } from "./ToggleThemeButton";
 
 export const Header = () => {
   return (
-    <header className="flex py-8">
-      <img
-        width={60}
-        height={60}
-        // src="/images/avatar.jpg"
-        src="/images/photo_profil.jpg"
-        alt="avatar"
-        className="top-[65px]  rounded-full shadow-lg md:absolute"
-        //md:absolute l'image passe en dessous du texte--
-      />
-      <span className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-2xl font-extrabold text-transparent drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)]">
+    <header className="flex items-center py-8">
+      <div className="flex flex-col items-center md:flex-row">
+        <img
+          width={60}
+          height={60}
+          // src="/images/avatar.jpg"
+          src="/images/photo_profil.jpg"
+          alt="avatar"
+          className="hidden rounded-full shadow-lg md:mr-6 md:block"
+          //md:absolute l'image passe en dessous du texte--
+        />
+        {/* <span className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-2xl font-extrabold text-transparent drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)]">
         {FULL_NAME}
-      </span>
+      </span> */}
+        <span className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-2xl font-extrabold text-transparent drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)]">
+          Welcome!
+        </span>
+      </div>
+
       <SocialNetworks className="ml-auto" socialNetworks={SOCIAL_NETWORKS} />
       <ToggleThemeButton />
     </header>
