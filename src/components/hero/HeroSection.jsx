4cc49d9b275b1156c23e2201aef0f3c2cac275bc
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-12-16 22:57:21 +0100

    modify robots.txt

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -17,7 +17,7 @@ export const HeroSection = () => {
           src="/images/photo_profil.jpg"
           alt="avatar guillaume gemelas"
           className="top-[-16px] right-[-42px] rounded-full shadow-lg md:absolute"
-          //md:absolute l'image passe en dessous du texte--
+          //md:absolute l'image passe en dessous du texte
         />
         {/* Hero - Exercise*/}
         <div className="flex flex-col gap-4 md:relative md:mr-32">
