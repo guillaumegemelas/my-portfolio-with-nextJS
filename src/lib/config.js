daf1625a2c2e4d773c9d2bcda52c3cf5a117b4ea
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2024-03-15 19:04:55 +0100

    first time

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,6 +3,7 @@ import {
   SiLinkedin,
   // SiTwitter
 } from "react-icons/si";
+import { FaFacebook } from "react-icons/fa";
 
 export const GITHUB_USERNAME = "guillaumegemelas";
 
@@ -11,11 +12,11 @@ export const EMAIL = "guillaumegemelas@gmail.com";
 export const FULL_NAME = "Guillaume Gemelas";
 
 export const SOCIAL_NETWORKS = [
-  // {
-  //   url: "https://www.twitter.com/",
-  //   name: "Twitter",
-  //   icon: <SiTwitter />,
-  // },
+  {
+    url: "https://www.facebook.com/people/Guillaume-Gemelas-aindev/61557617930908/",
+    name: "Facebook",
+    icon: <FaFacebook />,
+  },
   {
     url: "https://www.linkedin.com/in/guillaume-gemelas-a88880230/",
     name: "Linkedin",
