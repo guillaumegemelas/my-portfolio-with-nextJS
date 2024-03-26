b6090a11307fd22cddd16e93e43d9fe50bae34f8
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2024-03-26 13:20:21 +0100

    first time yes

--- src/components/atom/Typography.jsx
+++ src/components/atom/Typography.jsx
@@ -29,7 +29,7 @@ export const Typography = ({
           className,
           "text-5xl",
           color === "primary" ? "text-skin-primary" : "text-skin-secondary"
-          // "font-bold"
+          // "font-bold"..
         )}
       >
         {children}
