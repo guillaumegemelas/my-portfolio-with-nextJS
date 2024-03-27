463dcefc835a31fca8e9c0ec87b916f414942732
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2024-03-27 15:58:36 +0100

    first time captcha

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -177,7 +177,7 @@ export const Contact = () => {
           <ReCAPTCHA
             className="g-recaptcha"
             size="normal"
-            sitekey={import.meta.env.CAPTCHA_KEY}
+            sitekey={process.env.CAPTCHA_KEY}
             ref={recaptchaRef}
             onChange={onCaptchaChange}
           />
