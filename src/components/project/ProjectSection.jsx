a6ac77c0358c6ed868a205d5e1faa42f83609805
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2025-06-10 17:15:43 +0200

    add heder fixed 1

--- src/components/project/ProjectSection.jsx
+++ src/components/project/ProjectSection.jsx
@@ -90,7 +90,7 @@ export const ProjectSection = () => {
       }
     >
       <div className="flex flex-wrap justify-center gap-8">
-        {/* ----projet formprotech--------------*/}
+        {/* ----projet formprotech------------*/}
         <Project
           key="15"
           icon={<MdFactory />}
