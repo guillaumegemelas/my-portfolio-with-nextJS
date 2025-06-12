5bc930e37ff607c236467da45ef13910de90e3de
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2025-06-12 18:28:02 +0200

    add test commit port2

--- src/components/project/ProjectSection.jsx
+++ src/components/project/ProjectSection.jsx
@@ -103,7 +103,7 @@ export const ProjectSection = () => {
           url="https://formprotech.fr"
           //modif de l'uril (avant pas d'url, car sinon SEO pas ortpimisé, lien pas suivi)
         />
-        {/* ----projet clinicarmes-------------*/}
+        {/* ----projet clinicarmes-----------*/}
         <Project
           key="14"
           icon={<GiSawedOffShotgun />}
@@ -258,8 +258,8 @@ export const ProjectSection = () => {
         {/* <Project {...projects[0]} /> */}
       </div>
       {showAlert && (
-        <div className="fixed left-0 top-0 z-10 flex h-full w-full items-center justify-center">
-          <div className="absolute left-0 right-0 top-0 bg-red-500 p-2 text-center text-white opacity-100 transition-opacity duration-500">
+        <div className="fixed top-0 left-0 z-10 flex items-center justify-center w-full h-full">
+          <div className="absolute top-0 left-0 right-0 p-2 text-center text-white transition-opacity duration-500 bg-red-500 opacity-100">
             Le code source ne peut pas être visualisé.
           </div>
         </div>
